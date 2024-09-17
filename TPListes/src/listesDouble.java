4dd1b44b8cf05c9daa14aa7798d62ca2c33c31b4
ronan <ronan.garrigues@etu.univ-amu.fr>
2024-09-17 21:38:31 +0200

    Question 9

--- TPListes/src/listesDouble.java
+++ TPListes/src/listesDouble.java
@@ -3,34 +3,52 @@ public class listesDouble {
 	private DNode header, trailer;
 	private long size;
 	
+	public void setHeader(DNode header) {
+		this.header = header;
+	}
+	
+	public DNode getHeader() {
+		return this.header;
+	}
+	
+	public void setTrailer(DNode trailer) {
+		this.trailer = trailer;
+	}
+	
+	public DNode getTrailer() {
+		return this.trailer;
+	}
+	
+	public long getSize() {
+		return size;
+	}
+
+	public void setSize(long size) {
+		this.size = size;
+	}
+
 	private listesDouble() {
-		header = new DNode(null, null, null);
-		trailer = new DNode(null, header, null);
+		header = new DNode(0, null, null);
+		trailer = new DNode(0, header, null);
 		header.setNext(trailer);
-		size = 0;
+		setSize(0);
 	}
 	
-	public void addBegin(int valeur) {
-		DNode newNode = new DNode(valeur, header, null);
-		this.header = newNode;
-		++this.size;
-	}
-	
-	public String toString() {
-        DNode nodeProvisoire = this.header;       
-        String affichage = "[" + header.toString();
-        while (nodeProvisoire.getNext() != null) {
-            nodeProvisoire = nodeProvisoire.getNext();
-            affichage = affichage + ", "+ nodeProvisoire;
-        }
-        return (affichage + ']' +'\n');
+	public int nbNoeuds() {
+        DNode dNode = this.header;
+        int compteur = 1;
+        
+        while(dNode.getNext() != this.header) {
+            dNode = dNode.getNext();
+            ++compteur;
+        }    
+        return compteur;
     }
 	
 
 	
 	public static void main(String[] args) {
 		listesDouble liste = new listesDouble();
-		System.out.println(liste.toString());
 		}
 		
 	
